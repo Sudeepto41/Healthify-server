ba3583c940ebbd538c5aa82b41bf2798d49a5fe3
Sudeepto41 <sudeeptosaha.412@gmail.com>
2020-08-04 21:47:12 +0530

    initial

--- App.js
+++ App.js
@@ -1,45 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, _View } from 'react-native';
-import * as Fonts from 'expo-font'
-import { AppLoading } from 'expo'
-
-import Intro from './app/screens/intro'
+import { StyleSheet, Text, View } from 'react-native';
+import { Apploading } from 'expo';
+import * as Fonts from 'expo-font';
+import intro from './screens/intro'
 
 
 const getFonts = () => {
-    return Fonts.loadAsync({
-        'nunito-ExtraLight': require('./app/assets/fonts/NunitoSans-ExtraLight.ttf'),
-        'nunito-Light': require('./app/assets/fonts/NunitoSans-Light.ttf'),
-        'nunito-regular': require('./app/assets/fonts/NunitoSans-Regular.ttf'),
-        'nunito-SemiBold': require('./app/assets/fonts/NunitoSans-SemiBold.ttf'),
-        'nunito-Bold': require('./app/assets/fonts/NunitoSans-Bold.ttf'),
-        'nunito-ExtraBold': require('./app/assets/fonts/NunitoSans-ExtraBold.ttf'),
-        'nunito-Black': require('./app/assets/fonts/NunitoSans-Black.ttf')
-    });
+  return Fonts.loadAsync({
+    'nunito-regular': require('./assets/fonts/NunitoSans-Regular.ttf')
+  })
 }
+
 export default function App() {
-    const [fontsLoaded, setfontsLoaded] = useState(false)
 
-    if (fontsLoaded) {
-        return (
-            <Intro />
-        )
+  const [fontsLoaded, setfontsLoaded] = useState(false)
 
-    } else {
-        return (
-            <AppLoading
-                startAsync={getFonts}
-                onFinish={() => setfontsLoaded(true)}
-            />
-        )
-    }
+  if (fontsLoaded) {
+    return (
+      <intro />
+    )
+  } else {
+    return (
+      <Apploading
+        startAsync={getFonts()}
+        onFinish={() => setfontsLoaded(true)}
+      />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
+
--- app/config/color.js
+++
@@ -1,4 +0,0 @@
-export default {
-    bg_light: '#EEEEEE',
-    bg_dark: '2C2FC3',
-}
